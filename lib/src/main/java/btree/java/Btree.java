67db9f3e233731a713b4f5fa55ce425851a70c5a
woodsjm <jordan.nwc@gmail.com>
2022-11-21 05:53:00 +0000

    Add String constructor

--- lib/src/main/java/btree/java/Btree.java
+++ lib/src/main/java/btree/java/Btree.java
@@ -13,6 +13,10 @@ public class Btree {
         this.nodeValue = value;
     }
 
+    public void Btree(String value) {
+        this.nodeValue = value;
+    }
+
     public static void main(String[] args) {
         final String message = "\n"
             + "Btree-java is a port of the (Python) Binarytree library.\n"
