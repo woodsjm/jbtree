559613c731dfc6e38fdeae3a29d705725e9b1bd9
woodsjm <jordan.nwc@gmail.com>
2023-03-06 17:23:34 +0000

    Finish tree validation with letters

--- lib/src/test/java/btree/TreeValidateTest.java
+++ lib/src/test/java/btree/TreeValidateTest.java
@@ -8,10 +8,11 @@ import nl.altindag.log.LogCaptor;
 import nl.altindag.log.model.LogEvent;
 import org.junit.jupiter.api.Test;
 
+
 public class TreeValidateTest {
 
   @Test
-  void testTreeValidate() {
+  void testTreeValidateWithIntegers() {
     Btree.Node root = new Btree.Node(1);
     root.validate();
 
@@ -34,7 +35,30 @@ public class TreeValidateTest {
   }
 
   @Test
-  void testTreeValidateWithException() throws Exception {
+  void testTreeValidateWithLetters() {
+    Btree.Node root = new Btree.Node("A");
+    root.validate();
+
+    root = new Btree.Node("A");
+    root.setLeft(new Btree.Node("B"));
+    root.validate();
+
+    root = new Btree.Node("A");
+    root.setLeft(new Btree.Node("B"));
+    root.setRight(new Btree.Node(3));
+    root.validate();
+
+    root = new Btree.Node("A");
+    root.setLeft(new Btree.Node("B"));
+    root.setRight(new Btree.Node(3));
+    root.getLeft().setLeft(new Btree.Node(4));
+    root.getLeft().setRight(new Btree.Node(5));
+    root.getLeft().getRight().setLeft(new Btree.Node(6));
+    root.validate(); 
+  }
+
+  @Test
+  void testTreeValidateWithIntegersWithException() throws Exception {
     LogCaptor logCaptor = LogCaptor.forClass(Btree.class);
 
     Btree.Node root = new Btree.Node(1);
@@ -50,5 +74,24 @@ public class TreeValidateTest {
 
     assertTrue(capturedLogEvent.getThrowable().get().getClass() == BtreeException.NodeReferenceException.class);
     assertTrue(capturedLogEvent.getThrowable().get().getMessage().contains("cyclic reference at Node(1) (level-order index 4)"));
+  }
+
+  @Test
+  void testTreeValidateWithLettersWithException() throws Exception {
+    LogCaptor logCaptor = LogCaptor.forClass(Btree.class);
+
+    Btree.Node root = new Btree.Node("A");
+    root.setLeft(new Btree.Node("B"));
+    root.getLeft().setRight(root);
+
+    int statusCode = catchSystemExit(() -> {
+      root.validate();
+    });
+    assertEquals(0, statusCode);
+
+    LogEvent capturedLogEvent = logCaptor.getLogEvents().get(0);
+
+    assertTrue(capturedLogEvent.getThrowable().get().getClass() == BtreeException.NodeReferenceException.class);
+    assertTrue(capturedLogEvent.getThrowable().get().getMessage().contains("cyclic reference at Node(A) (level-order index 4)"));
   }
 }
